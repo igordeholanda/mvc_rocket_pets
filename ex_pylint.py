00d09af503e5c58359e2a4c04ed1499f097aecf7
Igor De Holanda <igor.rdholanda@gmail.com>
2024-12-11 18:00:55 -0300

    config: Adding Pylint to project

---
+++ ex_pylint.py
@@ -0,0 +1,12 @@
+
+
+print("Ola Mundo")
+
+
+def minha_funcao():
+    
+    print("Estou na minha funcao")
+
+# snake_case -> variáveis e funções
+# camelCase -> Não consta
+# PascalCase -> nomes de classes
